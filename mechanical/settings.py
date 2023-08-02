84f2f0c2801c250437b652e3827c61671192094a
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-08-02 22:21:04 +0300

    changes

--- mechanical/settings.py
+++ mechanical/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://mbackend-65aa08f37e31.herokuapp.com/','127.0.0.1' ]
 
 
 # Application definition
